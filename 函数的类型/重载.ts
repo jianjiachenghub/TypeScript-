c250655bd0e7e3cecf70ad636b2f51d0fa65f466
jianjiachengg <1797731542@qq.com>
2019-12-11 19:38:53 +0800

    重载运行的结果与文档不符

--- 函数的类型/重载.ts
+++ 函数的类型/重载.ts
@@ -1,7 +1,7 @@
 // 设置了返回值函数不能有没有返回的情况
 function reverse(x: number): number;
 function reverse(x: string): string;
-function reverse(x: number | string): number | string  { // TODO:这里报错 与文档说法不一致 return type does not include 'undefined'
+function reverse(x: number | string): number | string  { // TODO: 这里报错 与文档说法不一致 return type does not include 'undefined'
     if (typeof x === 'number') {
         return Number(x.toString().split('').reverse().join(''));
     } else if (typeof x === 'string') {
