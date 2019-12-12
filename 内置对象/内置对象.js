3c7d2f2c17ab1dcf69027613904ce8b432953123
jianjiachengg <1797731542@qq.com>
2019-12-12 14:16:03 +0800

    内置对象

---
+++ 内置对象/内置对象.js
@@ -0,0 +1,13 @@
+"use strict";
+//ECMAScript 的内置对象
+var b = new Boolean(1);
+var e = new Error('Error occurred');
+var d = new Date();
+var r = /[a-z]/;
+//DOM 和 BOM 的内置对象
+var body = document.body;
+var allDiv = document.querySelectorAll('div');
+document.addEventListener('click', function (e) {
+    // Do something
+});
+//# sourceMappingURL=内置对象.js.map
