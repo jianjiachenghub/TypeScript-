a1e53eaa57d7a7bf8be9a4741af7eaa7fcdceb75
jianjiachengg <1797731542@qq.com>
2019-12-12 20:37:46 +0800

    修改接口的描述

--- 对象的类型-接口/interface.ts
+++ 对象的类型-接口/interface.ts
@@ -1,7 +1,9 @@
-// 在 TypeScript 中，我们使用接口（Interfaces）来定义对象的类型。
-/* 什么是接口
-在面向对象语言中，接口（Interfaces）是一个很重要的概念，它是对行为的抽象，而具体如何行动需要由类（classes）去实现（implement）。
-TypeScript 中的接口是一个非常灵活的概念，除了可用于对类的一部分行为进行抽象以外，也常用于对「对象的形状（Shape）」进行描述。 */
+/**
+ * 在 TypeScript 中,使用接口（Interfaces）来定义对象的类型
+ * 它是对行为的抽象，而具体如何行动需要由类（classes）去实现（implement）。在类与接口的部分详细解释
+ * 除了可用于对类的一部分行为进行抽象以外，也常用于对「对象的形状（Shape）」进行描述。
+ * 这里介绍「对象的形状（Shape）」进行描述
+ */
 interface Person {
     name: string;
     age: number;
